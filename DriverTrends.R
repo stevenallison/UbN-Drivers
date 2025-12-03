2316df941d51caca3ddfbe46c45f08d06c0ceb2c
Steven Allison <stevenallison@dhcp-61-135.bio.uci.edu>
2025-12-03 15:04:23 -0800

    Update script

--- DriverTrends.R
+++ DriverTrends.R
@@ -1,6 +1,33 @@
 
+library(tidyverse)
+library(googledrive)
+library(googlesheets4)
 
-wood_weights <- drive_get("Wood_weights_native") %>% 
-  read_sheet(sheet = "wtf_native_harvest", col_names=T, trim_ws = T, 
-             na = c("","#N/A", "NA", "-", " ", NULL, "NULL"),
-             col_types = c("cnncccnDDLLLLLncLLnLncDLLLLLLLLLcLLLLLccLcLL")) # read in class for each column but will have to change as no option for factor and many columns are lists
+GDP <- drive_get("Trends in Drivers Data") %>% 
+  read_sheet(sheet = "GDP")
+
+
+pop <- read.csv("FAOSTAT_data_en_10-23-2025population.csv")
+pop <- pop[,which(colnames(pop)%in%c("Year","Element","Value"))]
+names(pop) [1] <- "Item"
+fert <- read.csv("FAOSTAT_data_en_10-23-2025fertilizers.csv")
+fert <- fert[,which(colnames(fert)%in%c("Year","Item","Value"))]
+pest <- read.csv("FAOSTAT_data_en_10-23-2025pesticides.csv")
+pest <- pest[,which(colnames(pest)%in%c("Year","Item","Value"))]
+subs <- read.csv("FAOSTAT_data_en_10-23-2025subsidies.csv")
+subs <- subs[,which(colnames(subs)%in%c("Year","Item","Value"))]
+
+pcgdp <- read.csv("A939RX0Q048SBEApcGDP.csv")
+pcgdp$Year <- year(pcgdp$observation_date)
+pcgdp$month <- month(pcgdp$observation_date)
+pcgdp <- pcgdp[pcgdp$month==1,]
+pcgdp$Item <- "GDPpc"
+pcgdp <- data.frame(Item=pcgdp$Item,Year=pcgdp$Year,Value=pcgdp$A939RX0Q048SBEA)
+
+df <- rbind(pop,fert,pest,subs,pcgdp)
+
+write.csv(df,"NNAtrends.csv",row.names=F)
+
+png("NNAdrivers.png",width=5,height=7,units="in",res=300)
+ggplot(df, aes(x=Year,y=Value)) + geom_line() + facet_wrap(~Item,scales="free_y",ncol=2) + theme_bw()
+dev.off()
