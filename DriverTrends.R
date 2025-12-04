5349335f948c1103dcea98e4f69c82888051fcc4
Steven Allison <stevenallison@dhcp-61-135.bio.uci.edu>
2025-12-04 11:09:45 -0800

    Update script

--- DriverTrends.R
+++ DriverTrends.R
@@ -4,26 +4,39 @@ library(googledrive)
 library(googlesheets4)
 library(gridExtra)
 library(gtable)
+library(grid)
 
 GDP <- drive_get("Trends in Drivers Data") %>% 
   read_sheet(sheet = "GDP") %>%
   pivot_longer(cols = GDPpc, names_to = "Variable")
 
 Population <- drive_get("Trends in Drivers Data") %>% 
-  read_sheet(sheet = "Population")
+  read_sheet(sheet = "Population") %>%
+  mutate(Variable = factor(Variable,levels = c("Total","Urban","Rural")))
 
 
 a <- ggplot(GDP, aes(x=Year, y=value)) +
+  xlim(1945, 2025) +
+  ylab("GDPpc ($US)") +
   geom_line() +
   theme_linedraw()
 
-b <- ggplot(Population, aes(x=Year, y=value)) +
-         geom_line() +
-         facet_wrap(~Variable, scales="free_y", ncol=1) +
-         theme_linedraw()
+b <- ggplot(Population, aes(x=Year, y=value, color = Variable)) +
+  xlim(1945, 2025) +
+  ylim(0,3.5e+08) +
+  ylab("Population size") +
+  geom_line() +
+  theme_linedraw() +
+  theme(legend.title = element_blank())
 
-g <- arrangeGrob(a, b, ncol=1, nrow=2)
-ggsave("Population.pdf", g, width = 9, height = 5)
+grob.a <- ggplotGrob(a)
+grob.b <- ggplotGrob(b)
+grob.c <- rbind(grob.a, grob.b, size = "first")
+grob.c$widths <- unit.pmax(grob.a$widths, grob.b$widths)
+grid.newpage()
+pdf("Population.pdf", width = 9, height = 5)
+grid.draw(grob.c)
+dev.off()
 
 pop <- read.csv("FAOSTAT_data_en_10-23-2025population.csv")
 pop <- pop[,which(colnames(pop)%in%c("Year","Element","Value"))]
